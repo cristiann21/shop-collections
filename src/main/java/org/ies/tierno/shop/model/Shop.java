39105d73666410a0b39543fe534bee2ade98bb50
Mikel <mikelsanvi@gmail.com>
2024-04-08 09:53:32 +0200

    ejercicios 1-4

--- src/main/java/org/ies/tierno/shop/model/Shop.java
+++ src/main/java/org/ies/tierno/shop/model/Shop.java
@@ -14,4 +14,34 @@ public class Shop {
     private String name;
     private Map<Integer, Product> productsById;
     private TreeSet<Customer> customers;
+
+
+    public Customer findCustomer(String nif) {
+        for (var customer : customers) {
+            if (customer.getNif().equals(nif)) {
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    public Order findCustomerOrder(String nif, int orderId) {
+        Customer customer = findCustomer(nif);
+        if (customer != null) {
+            for (var order : customer.getOrders()) {
+                if (order.getId() == orderId) {
+                    return order;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Product findById(int productId) {
+        if(productsById.containsKey(productId)) {
+            return productsById.get(productId);
+        } else {
+            return null;
+        }
+    }
 }
